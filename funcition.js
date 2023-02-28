5cf51f303e3f58906318c36dbbbe960985d95460
ayamjagoaneon <118793534+ayamjagoaneon@users.noreply.github.com>
2023-02-28 12:03:13 +0700

    Add files via upload

---
+++ funcition.js
@@ -0,0 +1,14 @@
+var sisi;
+var sisi;
+var total;
+
+function jumlahKelilingPersegi (a, b, c, d){
+    sisiA = a = 5;
+    sisiB = b = 5;
+    sisiC = c = 5;
+    sisiD = d = 5;
+    total = sisiA + sisiB + sisiC + sisiD
+    return total;
+}
+
+alert(jumlahKelilingPersegi(8, 3, 4, 5))
